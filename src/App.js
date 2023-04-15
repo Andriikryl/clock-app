08256b4c342d5846df108b164df1c12918685315
Andriikryl <andreyk557@gmail.com>
2023-04-15 22:05:19 +0300

    feat[binaryTime]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import "./App.css";
 
 function App() {
+  const binaryTime = () => {
+    const d = new Date();
+
+    const h = d.getHours();
+    const m = d.getMinutes();
+    const s = d.getSeconds();
+
+    const divider = (x) => [
+      Math.floor(x / 10)
+        .toString(2)
+        .padStart(4, "0"),
+      (x % 10).toString(2).padStart(4, "0"),
+      ,
+    ];
+
+    return [...divider(h), ...divider(m), ...divider(s)];
+  };
+
   return <div className="App"></div>;
 }
 
