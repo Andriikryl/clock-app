dd319eb7c8c47fb62a1588aa9800208909779f4e
Andriikryl <andreyk557@gmail.com>
2023-04-15 22:41:09 +0300

    feat[style-clock]

--- src/Binary.js
+++ src/Binary.js
@@ -1,11 +1,14 @@
 const Binary = ({ value }) => {
   return (
-    <div className="binary-clock">
-      <span className={value[0] === "1" ? "on" : "off"}></span>
-      <span className={value[1] === "1" ? "on" : "off"}></span>
-      <span className={value[2] === "1" ? "on" : "off"}></span>
-      <span className={value[3] === "1" ? "on" : "off"}></span>
-    </div>
+    <>
+      <div className="binary-clock">
+        <span className={value[0] === "1" ? "on" : "off"}></span>
+        <span className={value[1] === "1" ? "on" : "off"}></span>
+        <span className={value[2] === "1" ? "on" : "off"}></span>
+        <span className={value[3] === "1" ? "on" : "off"}></span>
+        <p>{parseInt(value, 2)}</p>
+      </div>
+    </>
   );
 };
 
